1f15b92cd82eac953716e5540d06bcec2555bb20
jester_adduru <jesteradduru@gmail.com>
2020-04-30 11:15:19 +0800

    code restructuring

--- app.js
+++ app.js
@@ -1,77 +1,5 @@
 google.charts.load("current", { packages: ["corechart"] });
-
 let covidDailyData = [["Day", "Active Cases", "Deaths", "Recoveries"]];
-getCovidCasesData();
-
-function drawChart() {
-  var data = google.visualization.arrayToDataTable(covidDailyData);
-
-  var options = {
-    title: "Philippines Covid 19 Curve Chart",
-    curveType: "none",
-    legend: { position: "bottom" },
-    chartArea: {
-      width: 500,
-    },
-  };
-
-  var chart = new google.visualization.LineChart(
-    document.getElementById("curve_chart")
-  );
-
-  chart.draw(data, options);
-}
-window.onresize = drawChart;
-// create function to return [day, cases, recoveries, deaths]
-
-function getCovidCasesData() {
-  fetch("https://coronavirus-ph-api.herokuapp.com/doh-data-drop")
-    .then((response) => response.json())
-    .then((data) => {
-      // start date - now loop
-      let countDailyCase = 0;
-      let countDailyCaseDeath = 0;
-      let countDailyCaseRecoveries = 0;
-      var now = new Date();
-      let lastIndex = 0;
-      let covidDate = "";
-      for (
-        var d = new Date("2020-01-30");
-        d <= now;
-        d.setDate(d.getDate() + 1)
-      ) {
-        for (let i = lastIndex; i < data.data.length; i++) {
-          let covidCaseDate = new Date(data.data[i].date_reported);
-          if (d.getTime() === covidCaseDate.getTime()) {
-            countDailyCase++;
-            if (data.data[i].date_died != "") {
-              countDailyCaseDeath++;
-            } else if (data.data[i].recovered_on != "") {
-              countDailyCaseRecoveries++;
-            }
-            lastIndex++;
-          } else {
-            lastIndex = i;
-            break;
-          }
-        }
-        covidDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-        covidDailyData.push([
-          covidDate,
-          countDailyCase,
-          countDailyCaseDeath,
-          countDailyCaseRecoveries,
-        ]);
-        // console.log(covidDailyData);
-      }
-    })
-    .then(() => {
-      google.charts.setOnLoadCallback(drawChart);
-    })
-    .catch((err) => console.log(err));
-}
-
-// total cases
 const cases = document.getElementById("cases");
 const casesToday = document.getElementById("cases-today");
 const deaths = document.getElementById("deaths");
@@ -83,28 +11,25 @@ const fatality = document.getElementById("fatality");
 const recovery = document.getElementById("recovery");
 const source = document.getElementById("source");
 const updated = document.querySelectorAll(".date-updated");
+let chartLastUpdated = "";
 
-getTotalCase();
-function getTotalCase() {
+async function getTotalCase() {
   totalCases(false);
-  fetch("https://coronavirus-ph-api.herokuapp.com/total")
-    .then((response) => response.json())
-    .then((data) => {
-      // console.log(data.data);
-      cases.innerHTML = data.data.cases;
-      deaths.innerHTML = data.data.deaths;
-      recoveries.innerHTML = data.data.recoveries;
-      admitted.innerHTML = data.data.admitted;
-      fatality.innerHTML = data.data.fatality_rate;
-      recovery.innerHTML = data.data.recovery_rate;
-      deathsToday.innerHTML = data.data.deaths_today;
-      recoveriesToday.innerHTML = data.data.recoveries_today;
-      casesToday.innerHTML = data.data.cases_today;
-      totalCases(true);
-      // source.setAttribute("href", data.source);
-      // source.innerHTML = data.source;
-      updated.forEach((update) => (update.innerHTML = data.data.last_update));
-    });
+  const res = await fetch("https://coronavirus-ph-api.herokuapp.com/total");
+  const data = await res.json();
+
+  cases.innerHTML = data.data.cases;
+  deaths.innerHTML = data.data.deaths;
+  recoveries.innerHTML = data.data.recoveries;
+  admitted.innerHTML = data.data.admitted;
+  fatality.innerHTML = data.data.fatality_rate;
+  recovery.innerHTML = data.data.recovery_rate;
+  deathsToday.innerHTML = data.data.deaths_today;
+  recoveriesToday.innerHTML = data.data.recoveries_today;
+  casesToday.innerHTML = data.data.cases_today;
+  updated.forEach((update) => (update.innerHTML = data.data.last_update));
+
+  totalCases(true);
 }
 
 function totalCases(visibility) {
@@ -119,3 +44,79 @@ function totalCases(visibility) {
     totalCases.style.display = "none";
   }
 }
+
+function drawChart() {
+  let data = google.visualization.arrayToDataTable(covidDailyData);
+
+  let options = {
+    title: "Philippines Covid 19 Curve Chart",
+    curveType: "none",
+    legend: { position: "bottom" },
+    series: {
+      0: { color: "#ffc40d" },
+      1: { color: "#ee1111" },
+      2: { color: "#00a300" },
+    },
+    chartArea: {
+      width: 500,
+    },
+  };
+
+  let chart = new google.visualization.LineChart(
+    document.getElementById("curve_chart")
+  );
+
+  chart.draw(data, options);
+}
+
+function importDataToChart(data) {
+  let countDailyCase = 0;
+  let countDailyCaseDeath = 0;
+  let countDailyCaseRecoveries = 0;
+  var now = new Date();
+  let lastIndex = 0;
+  let covidDate = "";
+
+  for (var d = new Date("2020-01-30"); d <= now; d.setDate(d.getDate() + 1)) {
+    for (let i = lastIndex; i < data.data.length; i++) {
+      let covidCaseDate = new Date(data.data[i].date_reported);
+      if (d.getTime() === covidCaseDate.getTime()) {
+        countDailyCase++;
+        if (data.data[i].date_died != "") {
+          countDailyCaseDeath++;
+        } else if (data.data[i].recovered_on != "") {
+          countDailyCaseRecoveries++;
+        }
+        lastIndex++;
+      } else {
+        lastIndex = i;
+        break;
+      }
+    }
+    let covidCaseDate = new Date(data.data[lastIndex - 1].date_reported);
+    if (covidCaseDate.getTime() >= d.getTime()) {
+      covidDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+      chartLastUpdated = covidDate;
+      covidDailyData.push([
+        covidDate,
+        countDailyCase,
+        countDailyCaseDeath,
+        countDailyCaseRecoveries,
+      ]);
+    }
+  }
+
+  document.getElementById("chart-last-updated").innerHTML = chartLastUpdated;
+}
+
+async function getCovidCasesData() {
+  const res = await fetch(
+    "https://coronavirus-ph-api.herokuapp.com/doh-data-drop"
+  );
+  const data = await res.json();
+  importDataToChart(data);
+  google.charts.setOnLoadCallback(drawChart);
+}
+
+getTotalCase();
+getCovidCasesData();
